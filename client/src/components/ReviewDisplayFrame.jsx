961c762f0071c8bc55ab1fc4b7688144e48339a5
mxbialostocki <matt.bialostocki@gmail.com>
2020-04-30 16:42:46 +1200

    randomise onClick

--- client/src/components/ReviewDisplayFrame.jsx
+++ client/src/components/ReviewDisplayFrame.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Grid } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Button, Typography, Grid } from '@material-ui/core'
 
 import Review from './Review'
 
 const ReviewDisplayFrame = ({ records }) => {
-
-  const randomTitle = Math.floor(Math.random() * records.length)
+  const randomTitle = () => {
+    return (
+      Math.floor(Math.random() * records.length)
+    )
+  }
+  const [titlesByDetermination, setTitlesByDetermination] = useState(randomTitle())
 
   return (
     <React.Fragment>
       <Grid container direction="column" justify="center" alignItems="center" >
-        <Review record={records[randomTitle]}/>
+        <Review record={records[titlesByDetermination]}/>
+        <Button textSizeLarge onClick={() => setTitlesByDetermination(randomTitle())}>
+          <Typography >not this one</Typography>
+        </Button>
       </Grid>
     </React.Fragment>
   )
