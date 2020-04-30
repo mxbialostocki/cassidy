4c4d2c86b8da344c87663f0cebae5c5f09796297
mxbialostocki <matt.bialostocki@gmail.com>
2020-04-30 16:14:10 +1200

    finalise stage 1 typography

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render } from 'react-dom'
 import 'semantic-ui-css/semantic.min.css'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 import App from './components/App.jsx'
