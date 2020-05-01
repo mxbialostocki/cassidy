05650de0022e19344fe79e24d6ddf3789ba90703
mxbialostocki <matt.bialostocki@gmail.com>
2020-05-01 20:01:29 +1200

    refactor relay config

--- relay.config.js
+++ relay.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  src: "client/src",
-  schema: "client/data/schema.graphql",
+  src: "./client/src",
+  schema: "./client/data/schema.graphql",
   exclude: [ "**/node_modules/**", "**/__mocks__/**", "**/__generated__/**" ]
 }
