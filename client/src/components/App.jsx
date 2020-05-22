fa97c5274e7af4ff72d6b6e9cfa9cc7b26efc5b8
mxbialostocki <matt.bialostocki@gmail.com>
2020-05-22 16:58:49 +1200

    render initial in HomePage

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,15 +4,15 @@ import { Route, Switch } from 'react-router-dom'
 // component imports
 import HomePage from './HomePage'
 import Daemon from './Daemon'
-import Main from './Main'
+// import Main from './Main'
 
 const App = () => {
   return (
     <React.Fragment>
       <Switch>
-        <Route exact path='/' component={HomePage} />
+        <Route exact path='/' render={props => <HomePage {...props} initial={true} />} />
+        <Route exact path='/:isbn' render={props => <HomePage {...props} initial={false} />} />
         <Route exact path='/daemon' component={Daemon} />
-        <Route path='/:isbn' component={Main} />
       </Switch>
     </React.Fragment>
   )
