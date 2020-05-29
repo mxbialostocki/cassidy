e1496c84c8463430c617de6125755845f795fc38
mxbialostocki <matt.bialostocki@gmail.com>
2020-05-29 13:20:52 +1200

    seek records at parent component

--- client/src/components/HomePage.jsx
+++ client/src/components/HomePage.jsx
@@ -4,11 +4,11 @@ import useStyles from '../lib/custom'
 import Landing from './Landing'
 import Main from './Main'
 
-const HomePage = ({ initial }) => {
+const HomePage = ({ records, initial }) => {
   const styles = useStyles()
 
   const [ isInitialLoad, setIsInitialLoad ] = useState(initial)
-  const activePage = isInitialLoad ? <Landing clickHandler={setIsInitialLoad}/> : <Main />
+  const activePage = isInitialLoad ? <Landing clickHandler={setIsInitialLoad}/> : <Main records={records}/>
 
   return (
     <Grid container direction="column" alignItems="center" className={styles.homePage}>
