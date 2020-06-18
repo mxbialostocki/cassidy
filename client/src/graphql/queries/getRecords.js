f40dad668a263bd83eb4bd92a9f4c3de79aa1c81
mxbialostocki <matt.bialostocki@gmail.com>
2020-06-18 12:07:16 +1200

    query comments

--- client/src/graphql/queries/getRecords.js
+++ client/src/graphql/queries/getRecords.js
@@ -1,5 +1,7 @@
 import { graphql } from 'react-relay'
 
+// this query is limited to five necessary fields to create reference points for the data - eg once you have the ISBNS in the app, then you can query them individually with a more verbose data set.
+
 const getRecordsQuery = graphql`
   query getRecordsQuery {
     records {
